54b65143503695848c0c59abd91ced2cc492758b
henryc99 <83266576+henryc99@users.noreply.github.com>
2021-05-04 14:10:03 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://henryc99.github.io/image-AI2/';
+let imageModelURL = 'https://henryc99.github.io/imageAI2/';
 
 // Video
 let video;
